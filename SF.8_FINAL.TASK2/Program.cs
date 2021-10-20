28f348e61ed517d6f8a52b15366da42d65b769d5
Irina Lokosova <persik2466@gmail.com>
2021-10-20 20:36:25 +0400

    Модуль 8. Итоговый проект 2. Подсчет общего объема папки со всеми вложенными каталогами и файлами.

--- SF.8_FINAL.TASK2/Program.cs
+++ SF.8_FINAL.TASK2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace SF._8_FINAL.TASK2
 {
@@ -6,7 +7,62 @@ namespace SF._8_FINAL.TASK2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите полное имя папки, размер которой требуется посчитать: ");
+            string path = Console.ReadLine();
+            if (!path.Contains(@":\"))
+            {
+                Console.WriteLine("Указан некорректный путь к папке: " + path);
+            }
+            else
+            {
+                var di = new DirectoryInfo(path);
+                try
+                {
+                    if (di.Exists)
+                    {
+                        //Запуск рекурсивного метода подсчета объема папки с файлами
+                        Console.WriteLine("Общий объем папки {0} со всеми вложенными файлами и каталогами составляет {1} байт", path, DirSize(di));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Каталога {0} не существует", path);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: {0}", ex.Message);
+                }
+            }
+            Console.ReadKey();
+
         }
+
+        /// <summary>
+        /// Рекурсивный метод подсчета объема папки с файлами
+        /// </summary>
+        /// <param name="d"></param>
+        public static long DirSize(DirectoryInfo d)
+        {
+            long size = 0;
+            FileInfo[] fil = d.GetFiles();
+
+            foreach (FileInfo file in fil)
+            {
+                size += file.Length;
+                Console.WriteLine("Файл: {0}, объем {1} байт", file.Name, file.Length);
+            }
+
+            DirectoryInfo[] direct = d.GetDirectories();
+
+            foreach (DirectoryInfo dir in direct)
+            {
+                Console.WriteLine("Каталог: {0}", dir.FullName);
+                size += DirSize(dir);
+                
+            }
+            
+            return size;
+        }
+
     }
 }
